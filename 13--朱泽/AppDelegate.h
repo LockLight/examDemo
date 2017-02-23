1b6788cb26c1e2e0ba4002eefe7bc0689d205148
LockLight <Locklightz@gmail.com>
2017-02-23 11:20:15 +0800

    Initial Commit

---
+++ 13--朱泽/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  13--朱泽
+//
+//  Created by locklight on 17/2/23.
+//  Copyright © 2017年 LockLight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 13--朱泽/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  13--朱泽
+//
+//  Created by locklight on 17/2/23.
+//  Copyright © 2017年 LockLight. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
